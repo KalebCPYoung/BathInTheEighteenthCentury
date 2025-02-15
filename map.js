1109d98edeb2c13853fd47f5d8e19ac8c118df8b
KalebCPYoung <kalebyoungh@gmail.com>
2025-02-15 23:23:02 +0000

    Update map.js

--- map.js
+++ map.js
@@ -4,26 +4,8 @@ export function createMap(containerId) {
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
-	
-	fetch('maps/map1085.tif')
-	.then(response => response.arrayBuffer())  // Convert the response to ArrayBuffer
-	.then(data => {
-			// Create a GeoRaster instance from the ArrayBuffer
-			const geoRaster = new GeoRaster(data);
-
-			// Create the GeoRasterLayer with the GeoRaster data
-			const geoRasterLayer = new GeoRasterLayer({
-					georaster: geoRaster,
-					opacity: 0.8
-			});
-
-			// Add the GeoRasterLayer to the map
-			geoRasterLayer.addTo(map);
-	})
-	.catch(error => {
-			console.error('Error loading GeoTIFF:', error);
-	});
-	
+	var map1735 = L.imageOverlay("map1735.png",[51.386482,-2.378191], [52.386482,-1.378191]);
+	map1735.addTo(map);
 	map.dragging.enable();
 	map.scrollWheelZoom.enable();
 	map.setMinZoom(13);
